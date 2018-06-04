1c0f4bdfaffd722e54fac1a2a2578d1e8b85b750
Samuel Mosey <samuelmosey1988@gmail.com>
2018-06-04 09:46:11 -0400

    second commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const map = [
-    "WWWWWWWWWWWWWWWWWWWWW",
+    "WWWWWWWWWWWWWWWWWWWWW", // [div, div, div, div, div]
     "W   W     W     W W W",
     "W W W WWW WWWWW W W W",
     "W W W   W     W W   W",
@@ -16,27 +16,94 @@ const map = [
     "WWWWWWWWWWWWWWWWWWWWW"
 ];
 
-let boxTop = 200;
-let boxLeft = 200;
-
-let main = document.querySelector("main");
+const grid = [];
+const player = document.getElementById("player")
+const main = document.querySelector("main");
 
 // This loop is creating a div for each column
 
-for (let column of map) {
-    const colElement = document.createElement("div")
-    colElement.classList.add("column")
-    main.appendChild(colElement)
-    
-// This loop is creating cell divs inside of the columns
-    
-    for (let cell of column) {
+for (let rowIndex in map) {
+    const mapRow = map[rowIndex];
+
+    const rowElement = document.createElement("div")
+    rowElement.classList.add("row")
+    main.appendChild(rowElement)
+    grid[rowIndex] = [];
+
+    // This loop is creating cell divs inside of the columns
+
+    for (let cellIndex in mapRow) {
+        const typeOfCell = mapRow[cellIndex];
+
         const cellElement = document.createElement("div")
         cellElement.classList.add("cell")
-        colElement.appendChild(cellElement)
-        if (cell === "W") {
+        rowElement.appendChild(cellElement)
+
+        // telling every cell its own position in grid, so that we can ask it later where it is
+        grid[rowIndex][cellIndex] = cellElement
+        cellElement.dataset.rowIndex = rowIndex
+        cellElement.dataset.cellIndex = cellIndex
+
+        if (typeOfCell === "S") {
+            cellElement.appendChild(player);
+        }
+        if (typeOfCell === "W") {
             cellElement.classList.add("wall")
         }
+        if (typeOfCell === "F") {
+            cellElement.classList.add("finish")
+        }
+        if (typeOfCell === "O") {
+            cellElement.classList.add("openSpace")
+        }
+        
     }
 }
 
+console.log(grid)
+
+
+document.addEventListener('keydown', (event) => {
+    if (event.key.indexOf("Arrow") === -1) return null
+
+    // this is where we ask the parent cell where it is in the grid
+    const currentRowIndex = Number(player.parentElement.dataset.rowIndex)
+    const currentCellIndex = Number(player.parentElement.dataset.cellIndex)
+
+    let targetCell = undefined
+
+    if (event.key === "ArrowRight") {
+        console.log("right")
+
+        targetCell = grid[currentRowIndex][currentCellIndex + 1]
+
+    } else if (event.key === "ArrowUp") {
+        console.log("up")
+
+        targetCell = grid[currentRowIndex - 1][currentCellIndex]
+
+    } else if (event.key === "ArrowDown") {
+        console.log("down")
+
+        targetCell = grid[currentRowIndex + 1][currentCellIndex]
+
+    } else if (event.key === "ArrowLeft") {
+        console.log("left")
+
+        targetCell = grid[currentRowIndex][currentCellIndex - 1]
+
+    }
+
+    if (targetCell) {
+
+        if (targetCell.className.indexOf("wall") === -1) {
+            targetCell.appendChild(player)
+        }
+
+        if (targetCell.className.indexOf("finish") !== -1) {
+            console.log("finish")
+            alert ("You win!!");
+        }
+    }
+
+});
